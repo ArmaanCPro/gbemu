c8b60236951df0bd4261bc99dbc6ac26ce350da4
Armaan Chahal <armaan.c2009@gmail.com>
2025-02-20 19:04:27 -0600

    Initial commit

---
+++ app/src/main.cpp
@@ -0,0 +1,10 @@
+#include "core.h"
+
+#include <iostream>
+
+int main()
+{
+    int x = core::test_return5();
+    std::cout << x << std::endl;
+    return 0;
+}
---
+++ core/src/core.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+namespace core 
+{
+    int test_return5()
+    {
+        return 5;
+    }
+}
