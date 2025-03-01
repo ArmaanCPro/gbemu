d72464d427d5b5509d776466b391d2d9e60bf2ca
armaanc <armaan.c2009@gmail.com>
2025-03-01 14:13:22 -0600

    fix issue in vertex/fragment shader string

--- core/src/fb_renderer.h
+++ core/src/fb_renderer.h
@@ -20,25 +20,27 @@ private:
     GLuint vao_id_, vbo_id_;
     GLuint shader_program_;
 
-    const GLchar* const vert_shader_ =
-        "#version 330 core"
-        "layout (location = 0) in vec2 aPos;"
-        "layout (location = 1) in vec2 aTexCoords;"
-        "out vec2 TexCoords;"
-        "void main()"
-        "{"
-        "   gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);"
-        "   TexCoords = aTexCoords;"
-        "}";
+    const GLchar* const vert_shader_ = R"(
+        #version 430 core
+        layout (location = 0) in vec2 aPos;
+        layout (location = 1) in vec2 aTexCoords;
+        out vec2 TexCoords;
+        void main()
+        {
+            gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);
+            TexCoords = aTexCoords;
+        }
+    )";
 
-    const GLchar* const frag_shader_ =
-        "#version 330 core"
-        "layout (location = 0) in vec2 aPos;"
-        "in vec2 TexCoords;"
-        "out vec4 FragColor;"
-        "uniform sampler2D screenTexture;"
-        "void main()"
-        "{"
-        "   FragColor = texture(screenTexture, TexCoords);"
-        "}";
+    const GLchar* const frag_shader_ = R"(
+        #version 430 core
+        layout (location = 0) in vec2 aPos;
+        in vec2 TexCoords;
+        out vec4 FragColor;
+        uniform sampler2D screenTexture;
+        void main()
+        {
+            FragColor = texture(screenTexture, TexCoords);
+        }
+    )";
 };
