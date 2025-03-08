a13220698ef0d9db3cea510dfd5e8e040148437d
Armaan Chahal <armaan.c2009@gmail.com>
2025-03-08 15:55:24 -0600

    change up the install_dependencies.sh script to include ninja and cmake as dependencies. also make the github action use apt-get instead of apt

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,22 +8,22 @@ command_exists() {
 # Update package list and install dependencies
 if command_exists apt-get; then
     sudo apt-get update
-    sudo apt-get install -y libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev pkg-config
+    sudo apt-get install -y libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev pkg-config cmake ninja-build
 elif command_exists yum; then
     sudo yum check-update
-    sudo yum install -y libXinerama-devel libXcursor-devel xorg-x11-server-devel mesa-libGLU-devel pkgconfig
+    sudo yum install -y libXinerama-devel libXcursor-devel xorg-x11-server-devel mesa-libGLU-devel pkgconfig cmake ninja-build
 elif command_exists pacman; then
-    sudo pacman -Syu
-    sudo pacman -S --needed libxinerama libxcursor xorg-server glu pkgconf
+    sudo pacman -Sy --needed
+    sudo pacman -S --needed libxinerama libxcursor xorg-server glu pkgconf cmake ninja
 elif command_exists zypper; then
     sudo zypper refresh
-    sudo zypper install -y libXinerama-devel libXcursor-devel xorg-x11-server-devel Mesa-libGLU-devel pkg-config
+    sudo zypper install -y libXinerama-devel libXcursor-devel xorg-x11-server-devel Mesa-libGLU-devel pkg-config cmake ninja
 elif command_exists dnf; then
     sudo dnf check-update
-    sudo dnf install -y libXinerama-devel libXcursor-devel xorg-x11-server-devel mesa-libGLU-devel pkgconf
+    sudo dnf install -y libXinerama-devel libXcursor-devel xorg-x11-server-devel mesa-libGLU-devel pkgconf cmake ninja-build
 elif command_exists emerge; then
     sudo emerge --sync
-    sudo emerge x11-libs/libXinerama x11-libs/libXcursor x11-base/xorg-server media-libs/mesa dev-util/pkgconfig
+    sudo emerge x11-libs/libXinerama x11-libs/libXcursor x11-base/xorg-server media-libs/mesa dev-util/pkgconfig dev-util/cmake dev-util/ninja
 else
     echo "Unsupported package manager. Please install the dependencies manually."
     exit 1
